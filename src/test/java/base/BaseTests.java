c2a911aaf5bae0e631c34628f039086ca5e7f168
Rafał <spectreclaud@gmail.com>
2021-01-18 15:25:43 +0100

    POM TAU Site https://the-internet.herokuapp.com/

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -6,6 +6,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
+import utils.WindowManager;
 
 import java.util.concurrent.TimeUnit;
 
@@ -34,5 +35,9 @@ public class BaseTests {
     @AfterClass
     public void tearDown(){
         driver.quit();
+    }
+
+    public WindowManager getWindowManager(){
+        return new WindowManager(driver);
     }
 }
